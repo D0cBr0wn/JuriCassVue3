8f0dbbb89c056a6c5e67de1ad24f0e1956659256
Nico Joret <nicolas.joret@vivaticket.com>
2023-05-09 16:32:41 +0200

    Localization Ok

--- JuricassVue3/vite.config.js
+++ JuricassVue3/vite.config.js
@@ -1,32 +1,32 @@
-import path from "path";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-const pathSrc = path.resolve(__dirname, "src");
+const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     fs: {
       // Allow serving files from one level up to the project root
-      allow: [".."],
-    },
+      allow: ['..']
+    }
   },
   plugins: [vue()],
   resolve: {
     alias: [
       {
-        find: "@",
-        replacement: pathSrc,
+        find: '@',
+        replacement: pathSrc
       },
       {
-        find: "@pages",
-        replacement: path.resolve(__dirname, "src/pages"),
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages')
       },
       {
-        find: "@model",
-        replacement: path.resolve(__dirname, "src/data/model"),
-      },
-    ],
-  },
-});
+        find: '@model',
+        replacement: path.resolve(__dirname, 'src/data/model')
+      }
+    ]
+  }
+})
