7ac6c8c605ecfd8e3d0a5b1d28b3f3a3eb9f48d3
Nico Joret <nicolas.joret@vivaticket.com>
2023-05-09 11:04:34 +0200

    fetch/display OK, need proper view

--- JuricassVue3/src/pages/index.vue
+++ JuricassVue3/src/pages/index.vue
@@ -1,15 +1,35 @@
 <template>
   <h1>home</h1>
   <button @click="$router.push('decision')">go</button>
-  <v-chip>{{ status.status }}</v-chip>
+  <div>
+    <v-list>
+      <v-list-item v-for="result in searchResult.results" :key="result.id">
+        <v-list-item-title>{{ result.decisionDate }}</v-list-item-title>
+        <v-list-item-subtitle>{{ result.jurisdiction }}</v-list-item-subtitle>
+        <v-list-item-subtitle>
+          {{ result.publication.join(' - ') }}
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>{{ result.formation }}</v-list-item-subtitle>
+        <v-list-item-title class="text--primary">{{ result.solution }}</v-list-item-title>
+        <v-list-item-subtitle>{{ result.summary }}</v-list-item-subtitle>
+        <v-row>
+          <v-col v-for="(theme, index) in result.themes" :key="index">
+            <v-chip>{{ theme }}</v-chip>
+          </v-col>
+        </v-row>
+        <v-btn :href="`decision/${result.id}`">Read</v-btn>
+      </v-list-item>
+    </v-list>
+  </div>
+  <v-alert type="error" text="This is an error bro !"></v-alert>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue'
-import { getHealth } from '@/controllers/healthController'
+import { search, searchResult } from '@/controllers/homeController'
 
 const status = ref({})
 onMounted(async () => {
-  status.value = await getHealth()
+  await search()
 })
 </script>
