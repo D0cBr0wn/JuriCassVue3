24cbedc28730f61bc0e0ce0a56bed43fc0dc490d
Nicolas Joret <nicolas.joret@impact-plus.fr>
2025-03-10 16:23:12 +0100

    upgrade packages

--- JuricassVue3/src/components/common/SkeletonLoader.vue
+++ JuricassVue3/src/components/common/SkeletonLoader.vue
@@ -1,11 +1,12 @@
 <template>
   <v-card v-for="repeat in repeats" class="skeleton">
-    <v-skeleton-loader type="article, actions"></v-skeleton-loader>
+    <pre> skeleton loader here</pre>
+    <!-- <v-skeleton-loader type="article, actions"></v-skeleton-loader> -->
   </v-card>
 </template>
 
 <script setup>
-import { VSkeletonLoader } from 'vuetify/labs/VSkeletonLoader'
+//import { VSkeletonLoader } from 'vuetify/labs/VSkeletonLoader'
 const props = defineProps({
   repeats: { type: Number, default: 10 }
 })
